6ab2bb81e673cdbab535df7b99adbd3d737f31a2
Nomad <pamirkantar21@gmail.com>
2020-10-05 15:42:16 -0700

    Standart interface implemented

---
+++ CBALInterface.js
@@ -0,0 +1,22 @@
+class CBALInterface {
+    constructor() {}
+
+    async getPrice(symbol) {
+        throw new Error("Must implement interface method!")
+    }
+
+    async getMultiplePrice(symbols) {
+        throw new Error("Must implement interface method!")
+    }
+
+    async getOrderBook(symbol, depth){
+        throw new Error("Must implement interface method!")
+    }
+
+    async getKlineData(symbol, interval, startTime, endTime, limit){
+        throw new Error("Must implement interface method!")
+    }
+
+}
+
+module.exports = CBALInterface
